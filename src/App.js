e0154f135c97739eb7a8c71afe7c2e3c41e2c7ec
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2019-07-07 19:31:12 -0700

    add state and componentDidMount fo for api call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+    state = {
+      data: []
+    }
+
+  componentDidMount() {
+      const API_KEY = 'c617e14926msh1b32a202714298ap130120jsn5a3fce0740f8';
+
+      fetch('https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf', {
+          headers : {
+          "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
+          "X-RapidAPI-Key": API_KEY
+          }
+        }
+      )
+      .then(response => response.json())
+      .then(data => console.log(data));
+
+      
+    }
+
   render() {
     return (
       <div>
-        
+  
       </div>
     );
   }
