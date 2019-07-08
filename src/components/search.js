110686fda49fc8250f6356b9d0dea21fac2cdce9
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2019-07-07 21:40:45 -0700

    move ComponentDidMount to search.js

--- src/components/search.js
+++ src/components/search.js
@@ -3,16 +3,43 @@ import React, { Component } from 'react';
 class Search extends Component {
 	
 	state = {
-		word: ''
+		word: '',
+		data: []
 	}
 
+	componentDidMount() {
+
+      const API_KEY = 'c617e14926msh1b32a202714298ap130120jsn5a3fce0740f8';
+
+      {!this.state.word ? console.log('Loading...'): fetch(`https://wordsapiv1.p.rapidapi.com/words/${this.state.word}/typeOf`, {
+          headers : {
+          "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
+          "X-RapidAPI-Key": API_KEY
+          }
+        }
+      )
+      .then(response => response.json())
+      .then(data => console.log(this.state.data));
+
+
+
+  }
+
+      
+      
+    }
+
 	getWord = (e) => {
-		this.setState({word: e.target.value});
+		//set state.word to the input value only when user presses enter
+		if(e.which === 13) {
+			this.setState({word: e.target.value});
+			console.log(this.state.word);
+		}
 	}
 
 	render() {
 		return (
-			<div><input type="text" name="wordSearch" onChange={this.getWord}/></div>
+			<div><input type="text" name="wordSearch" onKeyPress={this.getWord}/></div>
 		);
 	}	
 }
