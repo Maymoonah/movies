92c2f5677884218e53dd5cee2713749a541bf025
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2019-07-07 23:51:43 -0700

    remove componentDidMount, move api call to onSubmit function, add submit button

--- src/components/search.js
+++ src/components/search.js
@@ -7,39 +7,33 @@ class Search extends Component {
 		data: []
 	}
 
-	componentDidMount() {
-
-      const API_KEY = 'c617e14926msh1b32a202714298ap130120jsn5a3fce0740f8';
-
-      {!this.state.word ? console.log('Loading...'): fetch(`https://wordsapiv1.p.rapidapi.com/words/${this.state.word}/typeOf`, {
-          headers : {
-          "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
-          "X-RapidAPI-Key": API_KEY
-          }
-        }
-      )
-      .then(response => response.json())
-      .then(data => console.log(this.state.data));
-
-
-
-  }
-
-      
-      
-    }
-
 	getWord = (e) => {
-		//set state.word to the input value only when user presses enter
-		if(e.which === 13) {
-			this.setState({word: e.target.value});
-			console.log(this.state.word);
-		}
-	}
+		this.setState({word: e.target.value});
+		console.log(this.state.word);
 
+		}
+
+	onSubmit = (e) => {
+		console.log('clicked')
+		const API_KEY = 'c617e14926msh1b32a202714298ap130120jsn5a3fce0740f8';
+
+	    fetch(`https://wordsapiv1.p.rapidapi.com/words/${this.state.word}/typeOf`, {
+	      headers : {
+	      "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
+	      "X-RapidAPI-Key": API_KEY
+	      }
+	    }
+	    )
+	    .then(response => response.json())
+	    .then(data => console.log(data)); 
+	}
+	
 	render() {
 		return (
-			<div><input type="text" name="wordSearch" onKeyPress={this.getWord}/></div>
+			<div>
+				<input type="text" name="searchWord" onKeyUp={this.getWord} />
+				<button type="submit" onClick={this.onSubmit}>Search</button>
+			</div>
 		);
 	}	
 }
