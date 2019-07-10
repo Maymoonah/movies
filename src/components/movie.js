a292f6b5b6c209c71ffc6dc9dcbca6036342dade
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2019-07-10 15:16:28 -0700

    map through the movieData and append the movies to movie.js

--- src/components/movie.js
+++ src/components/movie.js
@@ -3,17 +3,22 @@ import React from 'react';
 const Movie = (props) => {
 	return (
 		<div className="container">
-			<div className="results">
-					<h4 className="text-center synonyms">Synonyms</h4>
-					{	
-						//map through synonyms array and append items to li
-						props.movieData.map((item)=> {
-							return (
-								<li key={item} className="list-item col-md-4">{item}</li>
-							)
-						})
-					}
-				</div>
+			<h4 className="text-center synonyms">Synonyms</h4>
+			{	
+				//poster, title, type, year, imdbID
+
+				//map through synonyms array and append items to li
+				props.movieData.map((item)=> {
+						return (
+							<div className="movieList row">
+								<div className="col-md-4">
+									<li key={item.ImdbID} className="list-item col-md-4">{item.Title}</li>
+									<img src={item.Poster} alt={item.Title}/>
+								</div>
+							</div>
+						)
+				})
+			}
 		</div>
 	);
 }
