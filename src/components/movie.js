1e43e451563d4e6df6cf5d08bae03d667127e59d
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2019-07-10 20:25:29 -0700

    add conditional rendering to title and images in movie

--- src/components/movie.js
+++ src/components/movie.js
@@ -16,8 +16,15 @@ const Movie = (props) => {
 							return (
 								<Col md={3} key={item.ImdbID} className="movieList">
 									<div className="col-md-4">
-										<h2 className="list-item col-md-4">{item.Title}</h2>
-										<img src={item.Poster} alt={item.Title}/>
+										{
+											item.Title.length < 15 ? <h6 className="list-item col-md-4">{item.Title}</h6> : 
+											<h6 className="movieTitle">{item.Title.substring(0, 15) + '...'}</h6>
+										}
+										{
+											item.Poster === "N/A" ? <img src="https://dummyimage.com/200x350&text=Picture+Not+Available" /> :	
+											<img src={item.Poster} alt={item.Title}/>
+										}
+										
 									</div>
 								</Col>
 							)
