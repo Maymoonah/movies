929e1c3efce02677e6cb4a746126ce8151b5f6a6
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2019-07-16 13:23:12 -0700

    add new file movie.js

---
+++ src/components/movie.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+
+const Movie = (props) => {
+	return (
+		<Container>
+			
+		</Container>
+	);
+}
+
+export default Movie;
